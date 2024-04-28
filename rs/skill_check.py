ad6f42754034c045ab9e4078f9bd368701283dc6
Aspirin2D <aspirin2d@outlook.com>
2024-04-28 09:46:05 +0800

    add sanity check status with tags

--- rs/skill_check.py
+++ rs/skill_check.py
@@ -5,29 +5,33 @@ from d20 import roll
 def sanity_check(metadata:dict) -> str:
     character = metadata["character"]
     sanity = character['sanity']
+
+    if "sanity_lost" not in character:
+        character["sanity_lost"] = 0
+    
+    if 'tags' not in character:
+            character['tags'] = []
     
     roll_num = roll("1d100").total
 
-    content = f"I am making a SANITY check against my own sanity:{sanity}."
+    content = f"I am making a SANITY check against my own sanity points: {sanity - character["sanity_lost"]}."
 
     if roll_num <=  sanity:
         return content + f"\nAnd... SUCCESS! I rolled {roll_num} against my sanity: {sanity}."
     
     lost = roll("1d6").total
-    character["sanity"] -= lost
+    
+    character["sanity_lost"] += lost
 
-    if "symptom" in metadata:
-        l = len(metadata["symptom"])
-        symptom =random.randrange(0, l)
-        symptom_str = metadata["symptom"][symptom]
-        
-        if "symptoms" not in character:
-            character["symptoms"] = []
+    if character["sanity"] - character["sanity_lost"] <= character["sanity"] // 2:
+        if "insane" not in character['tags']:
+            character['tags'].append("insane")
 
-        if symptom_str not in character["symptoms"]:
-            character['symptoms'].append(symptom_str)
+    content += f"\nAnd... FAILED! I lost {lost} sanity points."
+    
+    if lost >= 5:
+        content += f"\nI am shocked and lose control of my actions."
 
-    content += f"\nAnd... FAILED! I lost {lost} sanity points, and gained a symptom: {symptom_str}."
     return content
 
 def skill_check(skill:str, dc:Literal["easy", "normal", "hard"]|None, metadata: dict|None) -> str:
